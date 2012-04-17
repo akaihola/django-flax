b9cee7362f7e67e22403b27738745b9c3259fe98
Antti Kaihola <akaihol+github@ambitone.com>
2012-04-17 16:45:56 +0300

    Renamed from ambideploy to django-flax

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(
-    name = "ambideploy",
-    version = "0.1",
-    modules = ['ambideploy'],
+    name = 'flax',
+    version = '0.2',
+    modules = ['flax'],
 )
